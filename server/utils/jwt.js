da4e1648e03d3056b2ad660d650ef65a03f30ca5
Vrutik Sotha <vrutiksotha2005@gmail.com>
2026-06-29 22:47:59 +0530

    jwt.js minor changes

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,        // XSS protection
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
